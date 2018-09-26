81e57afcf206762aae37e5c66028bfa074e8185f
vikranthm <vkranth.mohanrao@gmail.com>
2018-09-26 11:02:07 +0530

    eureka

--- applications/allocations-server/src/main/java/io/pivotal/pal/tracker/allocations/App.java
+++ applications/allocations-server/src/main/java/io/pivotal/pal/tracker/allocations/App.java
@@ -7,9 +7,11 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.client.RestOperations;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 
 import java.util.TimeZone;
 
+@EnableCircuitBreaker
 @EnableEurekaClient
 @SpringBootApplication
 @ComponentScan({"io.pivotal.pal.tracker.allocations", "io.pivotal.pal.tracker.restsupport"})
